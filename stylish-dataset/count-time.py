56b325d81637572192f58e8ae12f9ccfb1d70fbd
Jonathon Duerig <>
2025-03-30 09:05:44 -0600

    Time utility scripts

---
+++ stylish-dataset/count-time.py
@@ -0,0 +1,30 @@
+# Accept a train or val list on standard input and split it into many
+# lists based on the duration of the clips.
+#
+# --wav is the directory of the wav files to check
+# --out is the output directory where a bunch of segment lists will be spawned
+
+import soundfile as sf
+import argparse, pathlib, re, sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--wav", default="wav/")
+args = parser.parse_args()
+
+wavdir = pathlib.Path(args.wav)
+
+already = {}
+time_bins = {}
+
+time = 0
+
+for line in sys.stdin:
+    fields = line.split("|")
+    if fields[0].strip() in already:
+        sys.stderr.write("DUPLICATE " + fields[0].strip())
+    else:
+        already[fields[0].strip()] = True
+    audio, sample_rate = sf.read(str(wavdir / fields[0].strip()))
+    time += audio.shape[0] / (300*80)
+
+print(time)
