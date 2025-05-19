2255a68e1195f657f96a9c18cb2afad9d35d2647
H.D.Tài <tai17082006.tk@gmail.com>
2025-05-19 18:53:22 +0700

    Add RMVPE pitch extractor

--- stylish-dataset/all-pitch.py
+++ stylish-dataset/all-pitch.py
@@ -3,16 +3,23 @@ from safetensors.torch import save_file
 from safetensors import safe_open
 import subprocess
 
+
 @click.command()
+@click.option(
+    "--method",
+    default="pyworld",
+    type=click.Choice(["pyworld", "rmvpe"], case_sensitive=False),
+)
 @click.option("--wavdir", default="wav", type=str)
 @click.option("--trainpath", default="train-list.txt", type=str)
 @click.option("--valpath", default="val-list.txt", type=str)
 @click.option("--outpath", default="pitch.safetensors", type=str)
 @click.option("--split", default=8, type=int)
-def main(wavdir, trainpath, valpath, outpath, split):
-    with open(trainpath, "r") as f:
+@click.option("--rmvpe_checkpoint", default=None, type=str)
+def main(method, wavdir, trainpath, valpath, outpath, split, rmvpe_checkpoint=None):
+    with open(trainpath, "r", encoding="utf-8") as f:
         trainlines = f.readlines()
-    with open(valpath, "r") as f:
+    with open(valpath, "r", encoding="utf-8") as f:
         vallines = f.readlines()
     lines = trainlines + vallines
     children = []
@@ -20,14 +27,50 @@ def main(wavdir, trainpath, valpath, outpath, split):
     begin = 0
     end = hop
     for i in range(split - 1):
-        with open(f"tmp-{i}.txt", "w") as f:
+        with open(f"tmp-{i}.txt", "w", encoding="utf-8") as f:
             f.write("".join(lines[begin:end]))
-        children.append(subprocess.Popen(["python", "stylish-dataset/single-pitch.py", "--wavdir", wavdir, "--inpath", f"tmp-{i}.txt", "--outpath", f"tmp-{i}.safetensors"]))
+        children.append(
+            subprocess.Popen(
+                [
+                    "python",
+                    "stylish-dataset/single-pitch.py",
+                    "--method",
+                    method,
+                    "--wavdir",
+                    wavdir,
+                    "--inpath",
+                    f"tmp-{i}.txt",
+                    "--outpath",
+                    f"tmp-{i}.safetensors",
+                ]
+                + ["--rmvpe_checkpoint", rmvpe_checkpoint]
+                if method == "rmvpe"
+                else []
+            )
+        )
         begin += hop
         end += hop
     with open(f"tmp-{split-1}.txt", "w") as f:
         f.write("".join(lines[begin:]))
-    children.append(subprocess.Popen(["python", "stylish-dataset/single-pitch.py", "--wavdir", wavdir, "--inpath", f"tmp-{split-1}.txt", "--outpath", f"tmp-{split-1}.safetensors"]))
+    children.append(
+        subprocess.Popen(
+            [
+                "python",
+                "stylish-dataset/single-pitch.py",
+                "--method",
+                method,
+                "--wavdir",
+                wavdir,
+                "--inpath",
+                f"tmp-{split-1}.txt",
+                "--outpath",
+                f"tmp-{split-1}.safetensors",
+            ]
+            + ["--rmvpe_checkpoint", rmvpe_checkpoint]
+            if method == "rmvpe"
+            else []
+        )
+    )
     for child in children:
         child.wait()
     result = {}
@@ -36,6 +79,7 @@ def main(wavdir, trainpath, valpath, outpath, split):
             for key in f.keys():
                 result[key] = f.get_tensor(key)
     save_file(result, outpath)
+
 
 if __name__ == "__main__":
     main()
--- stylish-dataset/single-pitch.py
+++ stylish-dataset/single-pitch.py
@@ -1,13 +1,10 @@
 import pathlib, sys
 
 import click
-import librosa
 import numpy
 import soundfile
 import torch
-import torchaudio
-import torchcrepe
-import pyworld
+import librosa
 
 from safetensors.torch import save_file
 
@@ -15,25 +12,44 @@ device = "cuda"
 
 
 @click.command()
+@click.option(
+    "--method",
+    default="pyworld",
+    type=click.Choice(["pyworld", "rmvpe"], case_sensitive=False),
+)
 @click.option("--wavdir", default="wav", type=str)
 @click.option("--inpath", default="list.txt", type=str)
 @click.option("--outpath", default="pitch.safetensors", type=str)
-def main(wavdir, inpath, outpath):
+@click.option("--rmvpe_checkpoint", default=None, type=str)
+def main(method, wavdir, inpath, outpath, rmvpe_checkpoint=None):
+    method = method.lower()
     wavdir = pathlib.Path(wavdir)
-    result = calculate_pitch(pathlib.Path(inpath), wavdir)
+    if method == "pyworld":
+        result = calculate_pitch_pyworld(pathlib.Path(inpath), wavdir)
+    elif method == "rmvpe":
+        assert (
+            rmvpe_checkpoint
+        ), "Pitch extraction method RVMPE requires a pretrained weight. Specify it with --rmvpe_checkpoint /path/to/pretrained/rmvpe/checkpoint"
+        result = calculate_pitch_rmvpe(pathlib.Path(inpath), wavdir, rmvpe_checkpoint)
     save_file(result, outpath)
 
 
-def calculate_pitch(path, wavdir):
+def calculate_pitch_pyworld(path, wavdir):
+    import pyworld
+
     result = {}
     count = 0
-    with path.open("r") as f:
+    with path.open("r", encoding="utf-8") as f:
         for line in f:
             fields = line.split("|")
             name = fields[0]
-            wave, sr = soundfile.read(wavdir / name)
-            if sr != 24000:
-                sys.stderr.write(f"Skipping {name}: Wrong sample rate ({sr})")
+            try:
+                wave, sr = soundfile.read(wavdir / name)
+                if sr != 24000:
+                    sys.stderr.write(f"Skipping {name}: Wrong sample rate ({sr})")
+            except:
+                sys.stderr.write(f"Skipping {name}: File not found or corrupted")
+                continue
             if wave.shape[-1] == 2:
                 wave = wave[:, 0].squeeze()
             time_bin = get_time_bin(wave.shape[0])
@@ -58,31 +74,8 @@ def calculate_pitch(path, wavdir):
             pitch = pyworld.stonemask(wave, f0, t, 24000)
             pitch = torch.from_numpy(pitch).float().unsqueeze(0)
             if torch.any(torch.isnan(pitch)):
-                pitch[torch.isnan(pitch)] = self.zero_value
+                pitch[torch.isnan(pitch)] = zero_value
 
-            # wave = librosa.resample(wave, orig_sr=sr, target_sr=16000)
-            #
-            # with torch.no_grad():
-            #    wave = torch.from_numpy(wave).float().unsqueeze(0)
-            #    fmin = 0
-            #    fmax = 8000
-            #    model = "full"
-            #    pitch, periodicity = torchcrepe.predict(
-            #        wave,
-            #        16000,
-            #        200,
-            #        fmin,
-            #        fmax,
-            #        model,
-            #        batch_size=8192,
-            #        device=device,
-            #        return_periodicity=True,
-            #    )
-            #    periodicity = torchcrepe.threshold.Silence(-60.0)(
-            #        periodicity, wave, 16000, 200
-            #    )
-            #    pitch = torchcrepe.threshold.At(0.21)(pitch, periodicity)
-            pitch = pitch[:, :-1]
             result[name] = pitch
             count += 1
             sys.stderr.write(".")
@@ -90,6 +83,58 @@ def calculate_pitch(path, wavdir):
                 sys.stderr.write(str(count) + "\n")
             sys.stderr.flush()
     return result
+
+
+def calculate_pitch_rmvpe(path, wavdir, checkpoint):
+    from rmvpe import RMVPE
+
+    rmvpe = RMVPE(checkpoint)
+    result = {}
+    zero_value = -10
+    with path.open("r", encoding="utf-8") as f:
+        for line in f:
+            fields = line.split("|")
+            name = fields[0]
+            wave, sr = soundfile.read(wavdir / name)
+            try:
+                wave, sr = soundfile.read(wavdir / name)
+                if sr != 24000:
+                    sys.stderr.write(f"Skipping {name}: Wrong sample rate ({sr})")
+            except:
+                sys.stderr.write(f"Skipping {name}: File not found or corrupted")
+                continue
+            if wave.shape[-1] == 2:
+                wave = wave[:, 0].squeeze()
+            time_bin = get_time_bin(wave.shape[0])
+            if time_bin == -1:
+                sys.stderr.write(f"Skipping {name}: Too short\n")
+                continue
+            frame_count = get_frame_count(time_bin)
+            pad_start = (frame_count * 300 - wave.shape[0]) // 2
+            pad_end = frame_count * 300 - wave.shape[0] - pad_start
+            wave = numpy.concatenate(
+                [numpy.zeros([pad_start]), wave, numpy.zeros([pad_end])], axis=0
+            )
+
+            wave_16k = librosa.resample(
+                wave, orig_sr=24000, target_sr=16000, res_type="kaiser_best"
+            )
+            pitch_rmvpe = (
+                torch.from_numpy(rmvpe.infer_from_audio(wave_16k)).float().unsqueeze(0)
+            )  # (1, frames)
+            pitch = torch.nn.functional.interpolate(
+                pitch_rmvpe.unsqueeze(1),  # (1, 1, frames)
+                size=frame_count,
+                mode="linear",
+                align_corners=True,
+            ).squeeze(
+                1
+            )  # (1, frames)
+            if torch.any(torch.isnan(pitch)):
+                pitch[torch.isnan(pitch)] = zero_value
+            # pitch = pitch[:, :-1]
+            result[name] = pitch
+            return result
 
 
 def get_frame_count(i):
